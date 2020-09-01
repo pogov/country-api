14c699145f83b4e7003b138ec3f71c332a8f3644
Maciej Pogorzelski <maciejpogorzelski@MacBook-Pro-Maciej.local>
2020-09-01 12:44:42 +0200

    test refactor

--- src/components/CountryCard/CountryCard.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import CountryCard from "./CountryCard";
-
-test("renders component", async () => {
-  const { getByTestId } = render(<CountryCard />);
-  expect(getByTestId(/country-card/i)).toBeInTheDocument();
-});
