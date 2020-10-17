5689798af51bfd4236e345a5f61c80deb4600470
Maciej Pogorzelski <maciejpogorzelski@MacBook-Pro-Maciej.local>
2020-10-17 22:51:01 +0200

    custom style for select

--- src/components/DropdownMenu/DropdownMenu.js
+++ src/components/DropdownMenu/DropdownMenu.js
@@ -1,46 +1,25 @@
 import React from "react";
-// import styles from "./DropdownMenu.module.css";
+import styles from "./DropdownMenu.module.css";
 import PropTypes from "prop-types";
 
-// import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 const DropdownMenu = ({ list, header, handler }) => {
-  // const [isVisible, setIsVisible] = React.useState(false);
   const handleClick = (e) => {
     handler(e);
-    // setIsVisible(false);
   };
 
-  // const dropdownRef = useRef(null);
-
-  // document.addEventListener("mousedown", (e) => {
-  //   if (!dropdownRef.current || dropdownRef.current.contains(e.target)) {
-  //     return;
-  //   }
-  //   setIsVisible(false);
-  // });
   return (
-    <select
-      // className={styles.menu}
-      onChange={(e) => handleClick(e)}>
-      <option
-        // className={styles.header}
-        value="">
-        {header}
-      </option>
-      {list.map((item) => {
-        return (
-          <option
-            key={item}
-            value={item}
-            // className={styles.listItem}
-          >
-            {item}
-          </option>
-        );
-      })}
-    </select>
+    <div className={styles.customSelect}>
+      <select onChange={(e) => handleClick(e)}>
+        <option value="">{header}</option>
+        {list.map((item) => {
+          return (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
+    </div>
   );
 };
 
